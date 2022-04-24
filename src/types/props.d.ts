33487e617887361aad3240ac01c22270a921608f
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-04-24 13:18:59 +0900

    SET

--- src/types/props.d.ts
+++ src/types/props.d.ts
@@ -1,8 +1,11 @@
-export type Props = {
-    posts: Array;
-    postDetail: Array;
-    id: string;
-    slug: string;
-    total: number;
-    currentNum: number;
+/* -------------------------------------------------------
+    ▽ Props の型定義纏め ▽
+---------------------------------------------------------- */
+type Props = {
+  posts: Array;
+  postDetail: Array;
+  id: string;
+  slug: string;
+  total: number;
+  currentNum: number;
 };
